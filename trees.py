d79e7e8e3c7c2d03c12dbc83fe1320fcab3d09ed
Jedivarun <varundinesh@gmail.com>
2023-01-28 19:14:37 -0500

    y

--- trees.py
+++ trees.py
@@ -1,28 +1,24 @@
 import streamlit as st
 import pandas as pd
-import pydeck as pdk 
+import matplotlib.pyplot as plt
 
-st.title('SF Trees')
-st.write('This app analyses trees in San Francisco using'
-         ' a dataset kindly provided by SF DPW')
+st.set_page_config(page_title="CSV Visualization App", page_icon=":chart_with_upwards_trend:", layout="wide")
 
-trees_df = pd.read_csv('trees.csv')
-trees_df.dropna(how='any', inplace=True)
+def main():
+    st.title("CSV Visualization App")
+    st.subheader("Upload your CSV file and select the columns to visualize")
+    uploaded_file = st.file_uploader("Choose a CSV file", type=["csv"])
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+        st.dataframe(df.head())
+        columns = st.multiselect("Select the columns to visualize", df.columns)
+        if columns:
+            st.subheader("Plotting the selected columns")
+            plt.figure()
+            for column in columns:
+                plt.plot(df[column])
+            st.pyplot()
 
-sf_initial_view = pdk.ViewState(
-    latitude=37.77,
-    longitude=-122.4,
-    zoom=11
-    )
+if __name__=='__main__':
+    main()
 
-sp_layer = pdk.Layer(
-    'ScatterplotLayer',
-    data = trees_df,
-    get_position = ['longitude', 'latitude'],
-    get_radius=30)
-
-st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/light-v9',
-    initial_view_state=sf_initial_view,
-    layers = [sp_layer]
-    ))
